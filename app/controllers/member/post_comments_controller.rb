85ba530addcc077e7c2db472a704a1975a067b45
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-04-26 12:28:17 +0000

    [create]comment_destroy_function

--- app/controllers/member/post_comments_controller.rb
+++ app/controllers/member/post_comments_controller.rb
@@ -1,9 +1,15 @@
-class PostCommentsController < ApplicationController
+class Member::PostCommentsController < ApplicationController
   def create
     recipe = Recipe.find(params[:recipe_id])
     comment = current_member.post_comments.new(post_comment_params)
     comment.recipe_id = recipe.id
     comment.save
+    redirect_to recipe_path(recipe)
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:recipe_id])
+    PostComment.find(params[:id]).destroy
     redirect_to recipe_path(recipe)
   end
 
